3a179925b81cfcc940234e2081e903b2935a57e1
Atolycs <111569596+atolycs@users.noreply.github.com>
2026-01-19 11:57:14 +0900

    feat: add options

--- lua/closet/commands.lua
+++ lua/closet/commands.lua
@@ -11,11 +11,11 @@ end
 local function autocmd()
 	vim.api.nvim_create_autocmd("TermClose", {
 		callback = function()
-			if vim.g.atolycs.closet.devterm then
+			if vim.g.atolycs_devterm then
 				vim.cmd("c")
 			end
 
-			vim.g.atolycs.closet.devterm = false
+			vim.g.atolycs_devterm = false
 		end,
 	})
 end
@@ -24,7 +24,7 @@ local function devterm()
 	vim.cmd("split")
 	vim.cmd("terminal")
 	vim.cmd("resize 5")
-	vim.g.atolycs.closet.devterm = true
+	vim.g.atolycs_devterm = true
 end
 
 function M.init()
